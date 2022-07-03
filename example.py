fe4bfbfaf0721a40864e42ec6a6c0d124d0cabf5
pn_jh95 <76778082+wjs2063@users.noreply.github.com>
2022-07-03 20:41:28 +0900

    Update example.py

--- example.py
+++ example.py
@@ -9,6 +9,8 @@ from airflow.operators.python import PythonOperator
 import datetime as dt
 from pathlib import Path
 import pandas as pd
+import pendulum
+import os.path 
 #객체의 인스턴스생성 
 API_KEY="dfzooeS9k4AVavvSf%2B13z0KUc3sFYqtCcRm1ISMQJlODSzZ7%2BmgR%2FTBYr1lSpiIOHp6VSONV6iKmkmx6EsITew%3D%3D"
 
@@ -36,11 +38,12 @@ weather_key={
 
     
 }
-
+#한국시간적용
+local_tz=pendulum.timezone("Asia/Seoul")
 
 dag=DAG(
     dag_id='download_weather_information',
-    start_date=dt.datetime(2022,7,2,0,0,0),
+    start_date=dt.datetime(2022,7,3,12,0,0,tzinfo=local_tz),
     schedule_interval=dt.timedelta(hours=1),
 )
 #1일전부터 시작
@@ -49,46 +52,46 @@ download_weathers_information=BashOperator(
     task_id="fetch_events",
     bash_command=(
         "mkdir -p /opt/airflow/dags/events && "
-        "curl --insecure -o /opt/airflow/dags/events/{{ds}}.json -L "
-        "https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst?serviceKey=dfzooeS9k4AVavvSf%2B13z0KUc3sFYqtCcRm1ISMQJlODSzZ7%2BmgR%2FTBYr1lSpiIOHp6VSONV6iKmkmx6EsITew%3D%3D&pageNo=1&numOfRows=1000&dataType=JSON&base_date={{ds_nodash}}&base_time={{'{:02}'.format(execution_date.hour)}}00&nx=55&ny=127"
+        "curl -k -o '/opt/airflow/dags/events/{{ds}}-{{'{:02}'.format(execution_date.hour)}}00.json' "
+        "'https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst?serviceKey=dfzooeS9k4AVavvSf%2B13z0KUc3sFYqtCcRm1ISMQJlODSzZ7%2BmgR%2FTBYr1lSpiIOHp6VSONV6iKmkmx6EsITew%3D%3D&pageNo=1&numOfRows=1000&dataType=Json&base_date={{ds_nodash}}&base_time={{'{:02}'.format(execution_date.hour) }}00&nx=55&ny=127'"
     ),
     dag=dag
 )
+
 
 def _get_weathers(**context):
     input_path=context["templates_dict"]["input_path"]
     output_path=context["templates_dict"]["output_path"] 
     Path(output_path).parent.mkdir(exist_ok=True)
     events=pd.read_json(input_path)
-    # dataframe 생성후
-    df=pd.DataFrame(columns=['baseDate',
-    'baseTime',
-    'nx',
-    'ny',
-    weather_key["PTY"],
-    weather_key["REH"],
-    weather_key['RN1'],
-    weather_key["T1H"],
-    weather_key['UUU'],
-    weather_key['VEC'],
-    weather_key['VVV'],
-    weather_key['WSD']
-    ])
+    if events['response']['header']['resultCode']!='00':
+        return -1
     temp=dict()
+    #계속 중복되는형태이므로 1번만 저장
+    # 변하는부분만 저장
     for x in events['response']['body']['items']['item']:
         temp['baseDate']=x['baseDate']
-        temp['baseTime']=x['baseTime']
-        temp[weather_key[x['category']]]=x['obsrValue']
+        temp['baseTime']='{:04d}'.format(int(x['baseTime']))
         temp['nx']=x['nx']
         temp['ny']=x['ny']
-    df.append(data=temp,ignore_index=True)
-    df.to_csv(output_path,index=False)
+        temp[x['category']]=x['obsrValue']
+    df1=pd.DataFrame(columns=temp.keys())
+    df1=df1.append(temp,ignore_index=True)
+    # 파일이 존재하면 
+    if os.path.isfile(output_path):
+        df=pd.read_csv(output_path,index_col=0)
+        df=pd.concat([df,df1],ignore_index=True)
+        df.to_csv(output_path)
+    else:
+        df1.to_csv(output_path,index=False)
+
+
 
 get_weathers=PythonOperator(
     task_id='get_weathers',
     python_callable=_get_weathers,
-    templates_dict={"input_path":"/tmp/data/events/{{ds}}.json",
-    "output_path":"/opt/airflow/dags/{{ds}}.csv"},
+    templates_dict={"input_path":"/opt/airflow/dags/events/{{ds}}-{{'{:02}'.format(execution_date.hour)}}00.json",
+    "output_path":"/opt/airflow/dags/results/{{ds}}.csv"},
     dag=dag,
 )
 
